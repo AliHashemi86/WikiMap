9d428be9937b19160f5690a8657c37cd584f1a26
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-02-12 13:40:21 -0500

    commit before pull

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,9 @@
 // Client facing scripts here
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+  console.log('HELLO!');
+});
+

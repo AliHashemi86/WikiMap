3e487c3517dffb9a5b5db5884f40b7f28a27d772
ShayFae <ifrahnoor108@gmail.com>
2022-02-13 18:15:53 -0500

    add user inputs for information

--- routes/new_map.js
+++ routes/new_map.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     console.log('TEST');
@@ -8,3 +9,24 @@ module.exports = (db) => {
   });
   return router;
 };
+
+///created map added to database?
+const createMap = function(maps) {
+  const queryString = `
+  INSERT INTO properties (user_id, title, description, image, location, created_at)
+  VALUES($1, $2, $3, $4, $5, $6)
+  RETURNING *;
+  `;
+  const queryParams = [
+    maps.user_id,
+    maps.title,
+    maps.description,
+    maps.image,
+    maps.location,
+    maps.created_at
+  ];
+
+  database.query(queryString, queryParams).then((res) => {
+     console.log(res.rows) });
+}
+exports.createMap = createMap;

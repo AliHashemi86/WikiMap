0a92bf485b10bf74cc9d7a76e928351d3c2a0f6a
ShayFae <ifrahnoor108@gmail.com>
2022-02-13 19:24:27 -0500

    working on newmap post

--- routes/new_map.js
+++ routes/new_map.js
@@ -1,32 +1,33 @@
 const express = require('express');
 const router  = express.Router();
 
-
 module.exports = (db) => {
   router.get("/", (req, res) => {
     console.log('TEST');
     res.render("new_map");
   });
+
+  router.post('/', (req, res) => {
+    const maps = req.body;
+    const queryString = `
+    INSERT INTO maps (title, description, image, location)
+    VALUES($1, $2, $3, $4)
+    RETURNING *;
+    `;
+    const queryParams = [
+      maps.title,
+      maps.description,
+      maps.image,
+      maps.location,
+    ];
+
+    return db.query(queryString, queryParams)
+    .then((data) => {
+      console.log('one')
+      console.log(data.rows)
+      res.json(data.rows);
+    })
+    .catch(err => console.log(err));
+  });
   return router;
 };
-
-///created map added to database?
-const createMap = function(maps) {
-  const queryString = `
-  INSERT INTO properties (user_id, title, description, image, location, created_at)
-  VALUES($1, $2, $3, $4, $5, $6)
-  RETURNING *;
-  `;
-  const queryParams = [
-    maps.user_id,
-    maps.title,
-    maps.description,
-    maps.image,
-    maps.location,
-    maps.created_at
-  ];
-
-  database.query(queryString, queryParams).then((res) => {
-     console.log(res.rows) });
-}
-exports.createMap = createMap;

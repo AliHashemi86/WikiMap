5c8cf595bce7d8c1c7ab8becea8cc66bf5293cfa
BryptoMensch <90989763+BryptoMensch@users.noreply.github.com>
2022-02-15 11:07:58 -0500

    Added cookie to site
    Added express-session cookie to site

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const session = require("express-session");
+const sessionConfig = {
+  name: "monster", // name of cookie
+  secret: process.env.SECRET, // secret that makes the cookie effective (find in ENV)
+  cookie: {
+    maxAge: 1000 * 60 * 60, // time span of cookie
+    secure: false, // for production, set to true for HTTPS only access
+    httpOnly: true, // true means no access from javascript
+  },
+  resave: false,
+  saveUnitialized: true, // doesn't ask users to consent, change in production.
+};
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -17,6 +29,8 @@ db.connect();
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(session(sessionConfig));
+
 app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
@@ -42,8 +56,7 @@ const loginRoutes = require("./routes/login");
 const new_mapRoutes = require("./routes/new_map");
 const mapPointsRoutes = require("./routes/mapPoints");
 
-const { getUersByEmail } = require('./public/scripts/helpers');
-
+const { getUersByEmail } = require("./public/scripts/helpers");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -53,7 +66,6 @@ app.use("/maps", mapsRoutes(db));
 app.use("/login", loginRoutes(db));
 app.use("/new_map", new_mapRoutes(db));
 app.use("/api/mapPoints", mapPointsRoutes(db));
-
 
 // Note: mount other resources here, using the same pattern above
 

0d4a89a24324de8482c6297101262b2244999e6d
ShayFae <ifrahnoor108@gmail.com>
2022-02-16 08:51:09 -0500

    local host was bugged

---
+++ db/seeds/04_favorites.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO profiles (user_id, map_id, favorite_id, contrbuted_to_map_id) VALUES (1, 1, 1, 1);
+-- INSERT INTO profiles (user_id, map_id, favorite_id, contrbuted_to_map_id) VALUES (2, 2, 2, 2);
+
+INSERT INTO favorites (user_id, map_id) VALUES (2, 2);
+INSERT INTO favorites (user_id, map_id) VALUES (1, 1);
